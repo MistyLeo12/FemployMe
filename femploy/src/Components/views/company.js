5cc39d68b6ca371213182ed68ebd96f1dccf3ce8
ninglinzhang <blaireninglinzhang@gmail.com>
2018-10-14 05:18:45 -0400

    tried more routing stuff but nothing's working

--- femploy/src/Components/views/company.js
+++ femploy/src/Components/views/company.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+
+
+
 export default class Company extends Component {
   render() {
     return (
--- femploy/src/routes.jsx
+++ femploy/src/routes.jsx
@@ -1,15 +1,15 @@
 //routes
 
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, Switch } from 'react-router';
 import App from './App';
 import Company from './Components/views/company.js';
 import Review from './Components/views/review.js';
-export default (
+export default () => (
  	<div>
  		<Route exact path='/' component={App}/>
- 		<Route path='company' component={Company}/>
- 		<Route path='review' component={Review}/>
+ 		<Route exact path='/company' component={Company}/>
+ 		<Route exact path='/review' component={Review}/>
  	</div>
     
   
