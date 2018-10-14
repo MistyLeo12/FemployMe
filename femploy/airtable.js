ef77040e158d0c1bfe160ab158b28f3d475370e1
gc126 <gc126@duke.edu>
2018-10-14 02:24:13 -0400

    Add airtable integration

--- femploy/airtable.js
+++ femploy/airtable.js
@@ -3,15 +3,14 @@ var base = new Airtable({apiKey: 'keyWvlXmSn55006bY'}).base('app7RMaUfSoIVwdde')
 
 base('user').select({
     // Selecting the first 3 records in Grid view:
-    maxRecords: 5,
+    maxRecords: 7,
     view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
     records.forEach(function(record) {
-        console.log(record.get('ID'), record.get('company')); 
+        console.log(record.get('ID'), record.get('company'), record.get('position'), record.get('salary'), record.get('age') , record.get('equal_treatment'));
     });
-
     // To fetch the next page of records, call `fetchNextPage`.
     // If there are more records, `page` will get called again.
     // If there are no more records, `done` will get called.
