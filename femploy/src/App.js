61ec990256ec6280300390c108b2aae99d597d77
ninglinzhang <blaireninglinzhang@gmail.com>
2018-10-14 11:36:08 -0400

    updated

--- femploy/src/App.js
+++ femploy/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CompanyGrid from './Components/CompanyGrid/CompanyGrid'; 
+import ChartGrid from './Components/ChartGrid/ChartGrid';
+import Difference from './Components/Difference/Difference'
 
 import './App.css';
 import myLogo from './myLogo.png';
@@ -15,8 +17,35 @@ import {
 } from 'react-router-dom'
 
 
-
 class App extends Component {
+  state = {
+    diffData: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      diffData: [
+      {
+        datasets: [
+          {
+            label: "Rating",
+            data: [
+              1000,
+              1250
+            ],
+            backgroundColor: [
+              'rgba(127, 191, 63, 0.6)',
+              'rgba(54, 162, 235, 0.6)'
+              //'rgba(255, 206, 86, 0.6)'
+            ]
+          }
+        ]
+      }
+    ]
+
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -24,14 +53,31 @@ class App extends Component {
 
           <div className="App-cover">
             <img src={myLogo} className="App-logo" alt="logo" width="470px" height="150"/>
-            <p>best coders</p>
+            <p>description</p>
           </div>
         </header>
         <div className="App-topCompanies">
-          <h2>Top Companies</h2>
+          <div className="myText">
+            <h2>Top Companies</h2>
+          </div>
         </div>
         <CompanyGrid>
         </CompanyGrid>
+
+
+
+        <div className="App-charts">
+          <ChartGrid>
+          </ChartGrid>
+        </div>
+
+
+        <div className ="App-diff"> 
+          {this.state.diffData.map(thing => {
+            return <Difference diffData={thing}/>
+          } ) }
+        </div>
+
       </div>
     );
   }
